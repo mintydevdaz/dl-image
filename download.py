d986b267db46d1de91a2a82fff7cbb485231abb6
mintydevdaz <mintydevdaz@proton.me>
2022-12-15 12:33:00 +1100

    Updated docstrings. Split funcs

--- download.py
+++ download.py
@@ -2,71 +2,95 @@ import os
 import sys
 import requests
 import validators
+import bs4
 from pathlib import Path
 from bs4 import BeautifulSoup
 
 
 def main():
-    # Ask user for valid URL
-    url = str(input("Enter URL: "))
-    valid_url = validate_url(url)
+    url = validate_url()
 
-    # Parse HTML
-    extract = get_url_data(valid_url)
+    extract = parse_request(get_request(url))
 
-    # Get list of images
     images = get_soup_image(extract)
 
-    # Exit if no images found.
     img_len = len(images)
-    if not img_len:
-        sys.exit("Unable to grab images from website.")
-    else:
-        print(f"Number of images found: {img_len}")
+    image_found(img_len)
 
-    # Ask user if they'll continue if high volume of images found
     if img_len >= 50:
         choice(img_len)
 
-    # Create folder
     new_folder = create_folder()
 
     # Download image files
     dl = download_images(url=url, images=images, filepath=new_folder)
 
-    # Show results. Delete newly created folder if no images downloaded.
     print(result(image_count=dl,
-                 list_of_images=img_len,
+                 image_num=img_len,
                  filepath=new_folder
                  ))
 
 
-def validate_url(url):
+def validate_url() -> str:
+    '''
+    Request URL from User. Check validity using
+    validators module. Exit program if invalid.
+
+    :return: valid URL
+    :rtype: str
+    '''
+    url = str(input("Enter URL: "))
     if not validators.url(url):
         sys.exit("Invalid URL. Try again.")
     return url
 
 
-def get_url_data(url):
+def get_request(url: str) -> requests.models.Response:
+    '''
+    Attempts to get response from URL.
+    Exits program if page can't be reached.
+    Exits program if there's a request timeout.
+
+    :param url: URL
+    :type url: str
+    :return: requests object
+    :rtype: requests.models.Response
+    '''
     try:
-        # Get content of url
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"} # noqa
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}  # noqa
         r = requests.get(url, headers=headers, timeout=5)
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
         sys.exit(f"Error reaching page! Additional details:\n** {e} **\n")
     except requests.exceptions.Timeout:
         sys.exit("Request timed out. Try again later.")
+    return r
 
+
+def parse_request(response: requests.models.Response) -> bs4.element.ResultSet:
+    '''
+    Finds and returns image tags
+
+    :param response: requests.get object
+    :type response: requests.models.Response
+    :return: Beautiful Soup object with html <img> tags
+    :rtype: bs4.element.ResultSet
+    '''
     # Parse HTML
-    soup = BeautifulSoup(r.text, features='lxml')
-
+    soup = BeautifulSoup(response.text, features='lxml')
     # Find image tags
     return soup.findAll('img')
 
 
-def get_soup_image(extract):
-    # Get list of images. Exit if error.
+def get_soup_image(extract: bs4.element.ResultSet) -> list[str]:
+    '''
+    Obtain list of 'src' URLS from <img> tags.
+
+    :param extract: Beautiful Soup object with html <img> tags
+    :type extract: bs4.element.ResultSet
+    :return: List of image 'src' URLs
+    :rtype: list[str]
+    '''
     img_1 = []
     img_2 = []
     img_3 = []
@@ -87,7 +111,16 @@ def get_soup_image(extract):
     return img_1 + img_2 + img_3 + img_4
 
 
-def choice(img_len):
+def image_found(img_len: int) -> None:
+    '''Exit if no images found else print image count to terminal'''
+    if not img_len:
+        sys.exit("Unable to grab images from website.")
+    else:
+        print(f"Number of images found: {img_len}")
+
+
+def choice(img_len: int) -> None:
+    '''Prompt User to exit program if too many images found'''
     while True:
         try:
             decision = str(input(
@@ -103,7 +136,14 @@ def choice(img_len):
             continue
 
 
-def create_folder():
+def create_folder() -> str:
+    '''
+    Create new folder in Desktop directory. Exit if Desktop path not found.
+    Continue prompt if filename already exists on Desktop.
+
+    :return: file path to Desktop
+    :rtype: str
+    '''
     while True:
         try:
             # Request Folder name
@@ -133,21 +173,15 @@ def download_images(url, images, filepath):
     # Attempt to obtain content of image
     for i, image in enumerate(images):
 
-        # Combine URL & image strings if http/s not found
-        if not image.startswith("http") or not image.startswith("https"):
-            image = f"{url}{image}"
+        # Combine URL & image strings if http(s) not found
+        image = check_http(url, image)
 
         try:
-            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"} # noqa
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"}  # noqa
             r = requests.get(image, headers=headers).content
-            name, ext = os.path.splitext(image)
 
             # Check file extension. Force .png if incorrect format.
-            html_ext = [
-                '.apng', '.gif', '.ico', '.jpg', '.jpeg', '.png', '.svg'
-            ]
-            if ext == "" or ext not in html_ext:
-                ext = ".png"
+            check_ext = check_extension(image)
 
             try:
                 # possibility of decode
@@ -155,7 +189,7 @@ def download_images(url, images, filepath):
 
             except UnicodeDecodeError:
                 # Download image after checking above condition
-                with open(f"{filepath}/image{i+1}{ext}", "wb+") as f:
+                with open(f"{filepath}/image{i+1}{check_ext}", "wb+") as f:
                     f.write(r)
 
                 # Track number of downloaded images
@@ -167,14 +201,44 @@ def download_images(url, images, filepath):
     return count
 
 
-def result(image_count, list_of_images, filepath):
+def check_http(url: str, image: str) -> str:
+    '''Modifies image URL if HTTP not present'''
+    if not image.startswith("http") or not image.startswith("https"):
+        return f"{url}{image}"
+    else:
+        return image
+
+
+def check_extension(image: str) -> str:
+    '''Modifies file extension if formatted incorrectly'''
+    name, ext = os.path.splitext(image)
+    html_ext = ['.apng', '.gif', '.ico', '.jpg', '.jpeg', '.png', '.svg']
+    if ext == "" or ext not in html_ext:
+        ext = ".png"
+    return ext
+
+
+def result(image_count: int, image_num: int, filepath: str) -> str:
+    '''
+    Prints final result of program to Command-Line.
+
+    :param image_count: Total count of succesfully downloaded images
+    :type image_count: int
+    :param image_num: Total count of images intially parsed from URL
+    :type image_num: int
+    :param filepath: Filepath to Desktop
+    :type filepath: str
+    :return: CLI response to User
+    :rtype: str
+    '''
+
     # Delete folder if no images downloaded
     if image_count == 0:
         Path(filepath).rmdir()
         return "Unable to download images. New folder deleted."
 
     # Show total images downloaded
-    if image_count == list_of_images:
+    if image_count == image_num:
         return f"All images downloaded!\nFiles saved in: {filepath}"
     else:
         return f"Images downloaded: {image_count}\nFiles saved in: {filepath}"
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,6 +1,6 @@
 import pytest
 from io import StringIO
-from download import validate_url, get_url_data, get_soup_image, choice
+from download import validate_url, get_request, get_soup_image, choice
 
 
 def test_validate_url():
@@ -10,29 +10,29 @@ def test_validate_url():
         assert validate_url('http://foobar.d')
     assert validate_url(
         'https://www.wikipedia.org'
-        ) == 'https://www.wikipedia.org'
+    ) == 'https://www.wikipedia.org'
     assert validate_url('http://10.0.0.1') == 'http://10.0.0.1'
 
 
 def test_get_url_data_HTTPError():
     with pytest.raises(SystemExit):
-        assert get_url_data('http://httpbin.org/status/403')
+        assert get_request('http://httpbin.org/status/403')
 
 
 def test_get_url_data_timeout():
     with pytest.raises(SystemExit):
-        assert get_url_data('http://httpbin.org/status/408')
+        assert get_request('http://httpbin.org/status/408')
 
 
 def test_get_url_data_soup():
     url = 'https://www.google.com'
-    test = get_url_data(url)
-    assert get_url_data(url) == test
+    test = get_request(url)
+    assert get_request(url) == test
 
 
 def test_get_soup_image():
     url = 'https://www.google.com'
-    test = get_url_data(url)
+    test = get_request(url)
     check = get_soup_image(test)
     assert get_soup_image(test) == check
 
